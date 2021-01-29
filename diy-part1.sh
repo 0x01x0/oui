42f202dad21de5dd26e99edff11d3cf32e5f904c
0x01x0 <60358707+0x01x0@users.noreply.github.com>
2021-01-29 17:55:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,4 +14,4 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git oui https://github.com/zhaojh329/oui.git' feeds.conf.default
